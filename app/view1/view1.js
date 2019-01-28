48f3580dc949b75d68fe8c1cb2e95200d65810c0
heyokha <39769714+heyokha@users.noreply.github.com>
2019-01-28 15:56:56 +0300

    .

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -10,9 +10,10 @@ angular.module('myApp.view1', ['ngRoute'])
 }])
 
 .controller('myCtrl', ['$http', '$scope', function($http, $scope) {
-  $scope.content = '',
+  $scope.content = '';
+  let things = [];
   $http({
-    url: 'https://ssdev.superagent.ru/TestApp/swagger/#/Values/GetWithParent',
+    url: 'https://ssdev.superagent.ru/TestApp/Values/GetWithParent',
     method: "GET",
     withCredentials: true,
     headers: {
@@ -22,8 +23,21 @@ angular.module('myApp.view1', ['ngRoute'])
   // $http.get('https://ssdev.superagent.ru/TestApp/swagger/#/Values/GetWithParent')
   .then(function(response) {
     $scope.content = response.data;
-    console.log($scope.response)
+    for (let i in response.data) {
+      let obj = response.data[i].skus
+      for (let item in obj) {
+        obj[item].group = response.data[i].group.name
+        console.log(obj[item].group)
+      }
+      for (let arr in obj){
+        things.push(obj[arr])
+      }
+      return things
+      console.log(things)
+    }
+    console.log(response)
   }, function(response){
     $scope.content = "Something went wrong";
   });
 }]);
+
